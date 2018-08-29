44d4484f5c37015c10c321425c3612e83214bd41
Camille Coti <coti@lipn.univ-paris13.fr>
2018-08-29 13:51:38 +0200

    main function with PAPI

--- minitest_haar.c
+++ minitest_haar.c
@@ -4,6 +4,16 @@
 #include <sys/param.h>
 #include <lapacke.h>
 
+#ifdef WITHPAPI
+#include <papi.h>
+#endif
+
+/* If PAPI does not work (indicates 0 counters):
+   echo 0 > /proc/sys/kernel/perf_event_paranoid
+*/
+
+#define NUMRUN 25
+
 #define DEFAULTM 16
 #define DEFAULTN 16
 
@@ -11,6 +21,16 @@ void ddiago( double* A, int M, int N, double cond );
 void dillrandom( double* A, int M, int N, int lda, double cond, double* work, double* work2 );
 void dhamt2_( double* restrict A, double* restrict B, double* restrict W, int M, int N, int lda, int ldb );
 void dgemm_( char*, char*, int*, int*, int*, double*, double*, int*, double*, int*, double*, double*, int* ); 
+
+
+#ifndef WITHPAPI
+extern __inline__ long long rdtsc(void) {
+  long long a, d;
+  __asm__ volatile ("rdtsc" : "=a" (a), "=d" (d));
+  return (d<<32) | a;
+}
+#endif
+
 
 void printmatrix( double* mat, int M, int N ) {
     int i, j;
@@ -43,7 +63,22 @@ int main( int argc, char** argv ){
     double* work;
     double* wor2;
     double cond = 1e10;
-
+    
+#ifdef WITHPAPI
+    #define NUM_EVENTS 3
+    int i, rc;
+    int events[NUM_EVENTS] = { PAPI_TOT_CYC, PAPI_L2_DCM, PAPI_L3_TCM };
+    long long values[NUM_EVENTS] = {0, 0, 0};
+    int num_hwcntrs = 0;
+    if ((num_hwcntrs = PAPI_num_counters()) <= PAPI_OK)  {
+        printf( "Not enough counters: %d available\n", num_hwcntrs );
+        return EXIT_FAILURE;
+    }
+#else
+    long long t_start, t_end;
+    int i;
+#endif
+    
     if( argc < 3 ) {
         M = DEFAULTM;
         N = DEFAULTN;
@@ -60,13 +95,33 @@ int main( int argc, char** argv ){
     
     dillrandom( mat, M, N, N, cond, work, wor2 );
     //    printmatrixOctave( mat, M, N );
-    printmatrix( mat, M, N );
+    // printmatrix( mat, M, N );
 
+#ifdef WITHPAPI
+        rc = PAPI_start_counters( events, NUM_EVENTS ); 
+        if( rc  != PAPI_OK ){
+            printf( "Error starting the counters\n" );
+        }
+#else
+        t_start = rdtsc();
+#endif
     /* Haar transform */
+    for( i = 0 ; i < NUMRUN ; i++ ) {
+        dhamt2_( mat, work, wor2, M, N, N, N );    
+        //    printmatrixOctave( work, M, N );
+        //         printmatrix( work, M, N );
+    }    
+#ifdef WITHPAPI
 
-    dhamt2_( mat, work, wor2, M, N, N, N );
-    //    printmatrixOctave( work, M, N );
-    printmatrix( work, M, N );
+    PAPI_stop_counters( values, NUM_EVENTS );
+    printf( "# M \t N \t PAPI_TOT_CYC \t PAPI_L2_DCM \t PAPI_L3_TCM\n" );
+    printf( "%d \t %d \t %lld \t %lld \t %lld\n", M, N, values[0] / NUMRUN, values[1] / NUMRUN , values[2] / NUMRUN );
+#else
+    t_end = rdtsc();
+    printf( "# M \t N \t time \n" );
+    printf( "%d \t %d \t %lld \n", M, N, (t_end - t_start ) / NUMRUN );
+
+#endif
 
     free( mat );
     free( work );
@@ -146,4 +201,3 @@ void ddiago( double* A, int M, int N, double cond ) {
     }
     
 }
-
